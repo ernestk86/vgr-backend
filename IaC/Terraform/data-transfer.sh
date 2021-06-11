0786582b15eb731e4de4d5cc0e460a91274245d1
Ernest Kim <52210251+ernestk86@users.noreply.github.com>
2021-06-11 18:11:31 -0400

    Finished adding github webhook

--- IaC/Terraform/data-transfer.sh
+++ IaC/Terraform/data-transfer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-host_path=/mnt/c/users/ernes/desktop/ernie_work/vgr-backend/iac/ansible/hosts
-dns_path=/mnt/c/users/ernes/desktop/ernie_work/vgr-backend/iac/ansible/new_dns
+host_path=/mnt/c/users/ernes/desktop/ernie_work/vgr-backend/forbidden/hosts
+dns_path=/mnt/c/users/ernes/desktop/ernie_work/vgr-backend/forbidden/new_dns
+github_webhook_path=/mnt/c/users/ernes/desktop/ernie_work/vgr-backend/forbidden/github_webhook
+github_token_path=/mnt/c/users/ernes/desktop/ernie_work/vgr-backend/forbidden/ght
 
 new_host=$(terraform output -raw instance_public_ip)
 echo $new_host >> $host_path
@@ -11,3 +13,11 @@ sudo rm $dns_path
 echo -n "http://" >> $dns_path
 echo -n $new_dns >> $dns_path
 echo -n ":8080" >> $dns_path
+
+sudo rm $github_webhook_path
+cat $dns_path >> $github_webhook_path
+echo -n "/github-webhook" >> $github_webhook_path
+github_webhook=$(<$github_webhook_path)
+github_token=$(<$github_token_path)
+
+curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer $github_token" https://api.github.com/repos/ernestk86/vgr-backend/hooks -d "{\"config\" : {\"url\" : \"$github_webhook\"}}"
